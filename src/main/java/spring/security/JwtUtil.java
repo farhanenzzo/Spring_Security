a2e2127adc55fa7f2c5fcffd9efa8358c47366c1
onthego <farhan.enzo99@gmail.com>
2024-04-23 14:07:34 +0600

    springboot_3.2.5

---
+++ src/main/java/spring/security/JwtUtil.java
@@ -0,0 +1,49 @@
+package spring.security;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.stereotype.Service;
+
+import java.security.Key;
+import java.util.Date;
+
+@Service
+public class JwtUtil {
+
+    private static final int expireInMs = 300 * 1000;
+
+    private final static Key key = Keys.secretKeyFor(SignatureAlgorithm.HS256);
+
+    public String buildToken(String username) {
+        return Jwts.builder()
+                .setSubject(username)
+                .setIssuer("farhan ahmed")
+                .setIssuedAt(new Date(System.currentTimeMillis()))
+                .setExpiration(new Date(System.currentTimeMillis() + expireInMs))
+                .signWith(key)
+                .compact();
+    }
+    public boolean validate(String token) {
+        return getUsername(token) != null && isExpired(token);
+    }
+
+    public String getUsername(String token) {
+        Claims claims = getClaims(token);
+        return claims.getSubject();
+    }
+
+    public boolean isExpired(String token) {
+        Claims claims = getClaims(token);
+        return claims.getExpiration().after(new Date(System.currentTimeMillis()));
+    }
+
+    private Claims getClaims(String token) {
+        return Jwts.parserBuilder()
+                .setSigningKey(key)
+                .build()
+                .parseClaimsJws(token)
+                .getBody();
+    }
+}
